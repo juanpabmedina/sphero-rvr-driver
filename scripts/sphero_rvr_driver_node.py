793ff1a6db95987e9594e7e06b348a57ceae5267
juanpabmedina <juanpab.m182@gmail.com>
2024-07-31 16:24:00 -0500

    first rvr leds configuration using ros

--- scripts/sphero_rvr_driver_node.py
+++ scripts/sphero_rvr_driver_node.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # license removed for brevity
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, ColorRGBA
+from geometry_msgs.msg import Vector3, Twist
 
 import sys
 import os
@@ -9,8 +10,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 import asyncio
 
-from sphero_sdk import SerialAsyncDal
 from sphero_sdk import SpheroRvrAsync
+from sphero_sdk import Colors
+from sphero_sdk import RvrLedGroups
+from sphero_sdk import SerialAsyncDal
 
 loop = asyncio.get_event_loop()
 rvr = SpheroRvrAsync(
@@ -19,18 +22,29 @@ rvr = SpheroRvrAsync(
     )
 )
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+class SpheroRVR():
+
+    def __init__(self) -> None:
+
+        # init ROS node
+        rospy.init_node("sphero_RVR_driver_node")
+
+    def create_ros_subscribers(self) -> None:
+
+        # Velocity 
+        rospy.Subscriber('sphero/leds', ColorRGBA, 
+                         self.cmd_leds_callback, queue_size=1)
+    
+    def cmd_leds_callback(self, msg) -> None:
+            rvr.set_all_leds(
+            led_group=RvrLedGroups.headlight_right.value,   # 0xe00
+            led_brightness_values=[msg.r, msg.g, msg.b]
+        )
+
+
         
 if __name__ == '__main__':
     try:
-        talker()
+        SpheroRVR()
     except rospy.ROSInterruptException:
         pass
