cab31b0ef88d4596fe987928096434cb183fdaff
juanpabmedina <juanpab.m182@gmail.com>
2024-07-31 17:33:44 -0500

    second rvr leds configuration using ros

--- scripts/sphero_rvr_driver_node.py
+++ scripts/sphero_rvr_driver_node.py
@@ -10,41 +10,43 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 import asyncio
 
-from sphero_sdk import SpheroRvrAsync
+from sphero_sdk import SpheroRvrObserver
 from sphero_sdk import Colors
 from sphero_sdk import RvrLedGroups
 from sphero_sdk import SerialAsyncDal
 
-loop = asyncio.get_event_loop()
-rvr = SpheroRvrAsync(
-    dal=SerialAsyncDal(
-        loop
-    )
-)
 
-class SpheroRVR():
+rvr = SpheroRvrObserver()
+# class SpheroRVR():
 
-    def __init__(self) -> None:
+#     def __init__(self) -> None:
 
-        # init ROS node
-        rospy.init_node("sphero_RVR_driver_node")
+#         # init ROS node
+#         rospy.init_node("sphero_RVR_driver_node")
 
-    def create_ros_subscribers(self) -> None:
+#     def create_ros_subscribers(self) -> None:
 
-        # Velocity 
-        rospy.Subscriber('sphero/leds', ColorRGBA, 
-                         self.cmd_leds_callback, queue_size=1)
+#         # Velocity 
+#         rospy.Subscriber('sphero/leds', ColorRGBA, 
+#                          self.cmd_leds_callback, queue_size=1)
     
-    def cmd_leds_callback(self, msg) -> None:
-            rvr.set_all_leds(
-            led_group=RvrLedGroups.headlight_right.value,   # 0xe00
-            led_brightness_values=[msg.r, msg.g, msg.b]
-        )
+#     def cmd_leds_callback(self, msg) -> None:
+#             rvr.set_all_leds(
+#             led_group=RvrLedGroups.headlight_right.value,   # 0xe00
+#             led_brightness_values=[msg.r, msg.g, msg.b]
+#         )
 
-
+def leds_callback(msg) -> None:
+    rvr.set_all_leds(
+    led_group=RvrLedGroups.headlight_right.value,   # 0xe00
+    led_brightness_values=[msg.r, msg.g, msg.b]
+)
         
 if __name__ == '__main__':
-    try:
-        SpheroRVR()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node("sphero_RVR_driver_node")
+    rospy.Subscriber('sphero/leds', ColorRGBA, leds_callback, queue_size=1)
+
+    rate=rospy.Rate(30)
+
+    while not rospy.is_shutdown():
+        rate.sleep()
