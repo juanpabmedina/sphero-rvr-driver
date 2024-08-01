f628467f005eb3f69b42da0fb95caf84c4fa3e08
juanpabmedina <juanpab.m182@gmail.com>
2024-08-01 17:06:30 -0500

    creating the driver

--- scripts/sphero_rvr_driver_node.py
+++ scripts/sphero_rvr_driver_node.py
@@ -15,41 +15,53 @@ from sphero_sdk import Colors
 from sphero_sdk import RvrLedGroups
 from sphero_sdk import SerialAsyncDal
 
+class SpheroRVR():
 
-rvr = SpheroRvrObserver()
-# class SpheroRVR():
+    def __init__(self) -> None:
 
-#     def __init__(self) -> None:
-
-#         # init ROS node
-#         rospy.init_node("sphero_RVR_driver_node")
-
-#     def create_ros_subscribers(self) -> None:
-
-#         # Velocity 
-#         rospy.Subscriber('sphero/leds', ColorRGBA, 
-#                          self.cmd_leds_callback, queue_size=1)
-    
-#     def cmd_leds_callback(self, msg) -> None:
-#             rvr.set_all_leds(
-#             led_group=RvrLedGroups.headlight_right.value,   # 0xe00
-#             led_brightness_values=[msg.r, msg.g, msg.b]
-#         )
-
-def leds_callback(msg) -> None:
-    rvr.set_all_leds(
-    led_group=RvrLedGroups.headlight_right.value,   # 0xe00
-    led_brightness_values=msg
-)
-        
-if __name__ == '__main__':
-    try: 
+        # init ROS node
         rospy.init_node("sphero_RVR_driver_node")
-        rospy.Subscriber('sphero/leds', Vector3, leds_callback, queue_size=1)
+        self.rvr = SpheroRvrObserver()
 
-        rate=rospy.Rate(30)
+    def create_ros_subscribers(self) -> None:
+        # Drive robot (velocity, heading, flags) 
+        rospy.Subscriber('rvr/drive', Vector3, self.drive_callback, queue_size=1)
+        # Leds
+        rospy.Subscriber('rvr/right_led', Twist, self.right_led_callback, queue_size=1)
+        rospy.Subscriber('rvr/left_led', Twist, self.left_led_callback, queue_size=1)        
 
-        while not rospy.is_shutdown():
-            rate.sleep()
+    def create_ros_publishers(self) -> None:
+        self.pub_battery_percentage = rospy.Publisher('rvr/battery', bool, queue_size=1)
+
+    def drive_callback(self, data):
+        # rospy.loginfo(data)
+        self.rvr.drive_with_heading(round(data.x), round(data.y), round(data.z))    
+
+    def right_led_callback(self,data):
+        # rospy.loginfo(data)
+        self.rvr.set_all_leds(
+            led_group=RvrLedGroups.headlight_right.value,   # 0xe00
+            led_brightness_values=[round(data.x),round(data.y),round(data.z)]
+        )
+
+    def left_led_callback(self,data):
+        rospy.loginfo(data)
+        self.rvr.set_all_leds(
+            led_group=RvrLedGroups.headlight_left.value,   # 0xe00
+            led_brightness_values=[round(data.x),round(data.y),round(data.z)]
+        )
+
+    def publish_battery_percentage(self):
+        battery_percentage_msg = self.rvr.get_battery_percentage()
+        self.pub_battery_percentage.publish(battery_percentage_msg)
+
+        
+if __name__ == "__main__":
+
+    try:
+        sphero_rvr = SpheroRVR()
+        rospy.spin()
+
     except rospy.ROSInterruptException:
-        pass
+        rospy.loginfo("Keyboard interrupted")
+        exit()
